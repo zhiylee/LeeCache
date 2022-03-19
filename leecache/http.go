7f5da0a2d55fa30f1417298f2ba7e697ee904432
Zhiyi.Lee <this@zhiy.me>
2022-03-19 15:12:41 +0800

    feature:add HTTP serve

---
+++ leecache/http.go
@@ -0,0 +1,58 @@
+package leecache
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strings"
+)
+
+type HTTPServe struct {
+	localhost string
+}
+
+func NewHTTPServe(host string) *HTTPServe {
+	return &HTTPServe{
+		localhost: host,
+	}
+}
+
+func (s *HTTPServe) Log(format string, v ...interface{}) {
+	log.Printf("[HTTPServe %s] %s", s.localhost, fmt.Sprintf(format, v...))
+}
+
+func (s *HTTPServe) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//log
+	s.Log("%s %s", r.Method, r.URL.Path)
+
+	// path : /
+	if r.URL.Path == "/" {
+		w.Write([]byte("[LeeCache] HTTP serve running."))
+		return
+	}
+
+	// path : /<groupName>/<key>
+	pathinfo := strings.SplitN(r.URL.Path[1:], "/", 2)
+	if len(pathinfo) != 2 {
+		http.Error(w, "bad request", http.StatusBadRequest)
+		return
+	}
+
+	groupName := pathinfo[0]
+	key := pathinfo[1]
+
+	group := GetGroup(groupName)
+	if group == nil {
+		http.Error(w, "no such group: "+groupName, http.StatusNotFound)
+		return
+	}
+
+	view, err := group.Get(key)
+	if err != err {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/octet-stream")
+	w.Write(view.ByteCopy())
+}
